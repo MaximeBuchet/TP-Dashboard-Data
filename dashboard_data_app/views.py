b5ce4c82de10be95a5df19804b205e3e6a6e2c36
MaximeBuchet <maxime.buchet.24@gmail.com>
2024-06-08 00:20:42 +0200

    ajout redirection login puis login vers page origin

--- dashboard_data_app/views.py
+++ dashboard_data_app/views.py
@@ -26,13 +26,13 @@ def login_view(request):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect("home")
+                return redirect(request.POST.get('redirect'))
             else:
                 form.add_error(None, "Invalid username or password")
     else:
         form = LoginForm()
 
-    return render(request, 'login.html', {'form': form})
+    return render(request, 'login.html', {'form': form, 'redirect' : request.GET.get('next', 'home')})
 
 def logout_view(request):
     logout(request)
